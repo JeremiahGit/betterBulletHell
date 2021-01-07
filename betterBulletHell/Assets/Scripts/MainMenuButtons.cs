d13cbdc70b14bd22e10124e24265c685d81893ae
Youngthanael <goza.jag@gmail.com>
2021-01-07 08:58:34 -0600

    Menu Buttons expand on hover and are ready to load things on click

---
+++ betterBulletHell/Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public bool isPlayArcade, isLevelSelect, isCharacterSelect, isSound, isExit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnMouseUp()
+    {
+        if (isPlayArcade) { } //Application.LoadLevel(1);
+        else if (isLevelSelect) { } //Application.LoadLevel(1);
+        else if (isCharacterSelect) { } //Application.LoadLevel(1);
+        else if (isSound) { } //Application.LoadLevel(1);
+        else if (isExit) { Application.Quit(); }
+    }
+}
