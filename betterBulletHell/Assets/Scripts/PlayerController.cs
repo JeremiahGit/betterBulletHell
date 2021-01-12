5e1f85e188308b95e9f862fd60b50c6089c064f9
Youngthanael <goza.jag@gmail.com>
2021-01-12 08:58:30 -0600

    started work on arcade

---
+++ betterBulletHell/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 50;
+    public float fireRate = 1;
+    private float xinput, yinput;
+
+    private Rigidbody2D rb;
+    private GameManager gm;
+    public GameObject bullet;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    void FixedUpdate()
+    {
+        //Movement and Player Input
+        xinput = Input.GetAxis("Horizontal");
+        yinput = Input.GetAxis("Vertical");
+        rb.velocity = new Vector2(xinput * speed * Time.deltaTime, yinput * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown("space"))
+        {
+            StartCoroutine("ShootLoop");
+        }
+    }
+
+    IEnumerator ShootLoop()
+    {
+        Instantiate(bullet, rb.position, Quaternion.Euler(0, 0, 0));
+        yield return new WaitForSeconds(fireRate * Time.deltaTime);
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(gameObject);
+    }
+}
