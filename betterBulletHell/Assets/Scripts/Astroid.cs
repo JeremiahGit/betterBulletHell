d43c6f5ef632f6a8943c11e8c34e2ca523f485fc
Youngthanael <goza.jag@gmail.com>
2021-01-15 08:58:33 -0600

    astroid sprites and script work

--- betterBulletHell/Assets/Scripts/Astroid.cs
+++ betterBulletHell/Assets/Scripts/Astroid.cs
@@ -10,16 +10,31 @@ public class Astroid : MonoBehaviour
     public float xRightBound;
     public float yLevel;
 
+    public Sprite warning;
+    public Sprite astroidOne;
+    public Sprite astroidTwo;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         transform.position = new Vector3(Random.Range(xLeftBound, xRightBound), yLevel);
+
+        int selector = Random.Range(0, 2);
+        if (selector == 0)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = astroidOne;
+        }
+        else 
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = astroidTwo;
+
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
