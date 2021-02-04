525940abf05fc1697ed5ef32c288d4b5276b7274
Youngthanael <goza.jag@gmail.com>
2021-02-04 08:58:07 -0600

    added score in arcade mode

--- betterBulletHell/Assets/Scripts/GameManager.cs
+++ betterBulletHell/Assets/Scripts/GameManager.cs
@@ -10,28 +10,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject[] enemyList; //Fill with prefabs
+    private int score;
+
+    public TextMeshProUGUI scoreText; 
 
 
-    // Start is called before the first frame update
+
 
     void Awake()
     {
-        QualitySettings.vSyncCount = 0;  // VSync must be disabled
-        Application.targetFrameRate = 60;
+        QualitySettings.vSyncCount = 0;   // VSync must be disabled
+        Application.targetFrameRate = 60; //IDK if these two lines are actually doing anthing \_(owo)_/
     }
 
     void Start()
     {
-        
+        score = 0;
+        StartCoroutine("SlowScoreIncrease");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+    IEnumerator SlowScoreIncrease()
+    {
+        while (true)
+        {
+            UpdateScore(1);
+            yield return new WaitForSeconds(.69f);
+        }
     }
 }
