5d287b06273ca7329767bb242397d771c96a783d
Youngthanael <goza.jag@gmail.com>
2021-03-23 07:38:42 -0500

    Added music, slider, and fixxed bug that would play duplicated music tracks, fixxed bug where music doesnt play on game start. TODO: Fix volume slider to remember position. Not critical

---
+++ betterBulletHell/Assets/Scripts/Sound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+    static bool isPlaying = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isPlaying != true)
+        {
+            isPlaying = true;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
